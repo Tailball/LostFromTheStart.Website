022fd3a5b0d073736254977c140adcd9dfa22299
Jochen <jochen.panjaer@ziango.com>
2018-08-20 18:43:12 +0200

    fucking facebook

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,10 +13,10 @@ const Navbar = () => {
                 <a href="https://www.instagram.com/lostfromthestartband/" target="_blank">
                     <FontAwesomeIcon className="icon" icon={faInstagram} />
                 </a>
-                <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fopen.spotify.com%2Fartist%2F6AVT9tIkQ0U18vDZbsIoha%3Fsi%3D4ya5wP8CTJqmVOfGFoLUTw&h=AT2gaWCG0lSPpI5Vu4CaONfe_f39zQYo4vDOzrXsioDcDXpSdzajZuL0iU0B-GL1gA160quyfgffaXZVM0GVgwQx2SW4dx84XHLtMxhxaM6fxUi6TjAEEm7r1JByQIf32xwJYy-4_WwCX94ua939fAUnpQ" target="_blank">
+                <a href="https://open.spotify.com/artist/6AVT9tIkQ0U18vDZbsIoha?si=4ya5wP8CTJqmVOfGFoLUTw" target="_blank">
                     <FontAwesomeIcon className="icon" icon={faSpotify} />
                 </a>
-                <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPLdIGABl3LGR_zbTUdMHIGFmipVeiWadQV&h=AT3OiKkN2reiq4yioCGDu1dZ-cRqNRCkhnmVls2TQvFCrLthYQ94AxeP79tsjVFKXgr0kKYHVBHDFmf50hXz0Cv6htopnqTzbIxtDTUDYhTCZvrSIMZt0znnoqogwXunkEZg9t4G5DqCSU5LrrYIFRviLg" target="_blank">
+                <a href="https://www.youtube.com/playlist?list=PLdIGABl3LGR_zbTUdMHIGFmipVeiWadQV" target="_blank">
                     <FontAwesomeIcon className="icon" icon={faYoutubeSquare} />
                 </a>
             </div>
