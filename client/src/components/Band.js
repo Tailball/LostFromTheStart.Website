fe416ed6d9ac8faf5aecf5373d83750c048be948
Jochen <jochen.panjaer@ziango.com>
2018-08-21 17:45:52 +0200

    mobile version of band page

--- client/src/components/Band.js
+++ client/src/components/Band.js
@@ -97,11 +97,11 @@ class Band extends React.Component {
                 <div className="featuring">
                     <h2>Featuring...</h2>
                     <ul>
-                        <li onClick={this.changeMember} data-member="hilde">Hilde - Mic</li>
-                        <li onClick={this.changeMember} data-member="seppe">Seppe - 6-string</li>
-                        <li onClick={this.changeMember} data-member="jochen">Jochen - 6-string</li>
-                        <li onClick={this.changeMember} data-member="robby">Robby - 4-string</li>
-                        <li onClick={this.changeMember} data-member="laurens">Laurens - Sticks</li>
+                        <li onClick={this.changeMember} data-member="hilde">Hilde<br />Mic</li>
+                        <li onClick={this.changeMember} data-member="seppe">Seppe<br />6-string</li>
+                        <li onClick={this.changeMember} data-member="jochen">Jochen<br />6-string</li>
+                        <li onClick={this.changeMember} data-member="robby">Robby<br />4-string</li>
+                        <li onClick={this.changeMember} data-member="laurens">Laurens<br />Sticks</li>
                     </ul>
                 </div>
                 <div className="bandmember">
@@ -109,9 +109,64 @@ class Band extends React.Component {
 
                     <div className="bandmember-description">
                         <h3>{this.state.selectedMember.name}</h3>
-                        <p>{this.state.selectedMember.description}</p>
-                        <p>Influenced by: {this.state.selectedMember.influences}</p>
-                        <p>Fun fact: {this.state.selectedMember.funFact}</p>
+                        <p className="bandmember-description-description">{this.state.selectedMember.description}</p>
+                        <p className="bandmember-description-influence">Influenced by: {this.state.selectedMember.influences}</p>
+                        <p className="bandmember-description-funfact">Fun fact: {this.state.selectedMember.funFact}</p>
+                    </div>
+                </div>
+
+                <div className="bandmember-mobile">
+                    <img src={bandmembers.Hilde.img} />
+
+                    <div className="bandmember-mobile-description">
+                        <h3>{bandmembers.Hilde.name}</h3>
+                        <p className="bandmember-mobile-description-description">{bandmembers.Hilde.description}</p>
+                        <p className="bandmember-mobile-description-influence">Influenced by<br />{bandmembers.Hilde.influences}</p>
+                        <p className="bandmember-mobile-description-funfact">Fun fact<br />{bandmembers.Hilde.funFact}</p>
+                    </div>
+                </div>
+
+                <div className="bandmember-mobile">
+                    <img src={bandmembers.Seppe.img} />
+
+                    <div className="bandmember-mobile-description">
+                        <h3>{bandmembers.Seppe.name}</h3>
+                        <p className="bandmember-mobile-description-description">{bandmembers.Seppe.description}</p>
+                        <p className="bandmember-mobile-description-influence">Influenced by<br />{bandmembers.Seppe.influences}</p>
+                        <p className="bandmember-mobile-description-funfact">Fun fact<br />{bandmembers.Seppe.funFact}</p>
+                    </div>
+                </div>
+
+                <div className="bandmember-mobile">
+                    <img src={bandmembers.Jochen.img} />
+
+                    <div className="bandmember-mobile-description">
+                        <h3>{bandmembers.Jochen.name}</h3>
+                        <p className="bandmember-mobile-description-description">{bandmembers.Jochen.description}</p>
+                        <p className="bandmember-mobile-description-influence">Influenced by<br />{bandmembers.Jochen.influences}</p>
+                        <p className="bandmember-mobile-description-funfact">Fun fact<br />{bandmembers.Jochen.funFact}</p>
+                    </div>
+                </div>
+
+                <div className="bandmember-mobile">
+                    <img src={bandmembers.Robby.img} />
+
+                    <div className="bandmember-mobile-description">
+                        <h3>{bandmembers.Robby.name}</h3>
+                        <p className="bandmember-mobile-description-description">{bandmembers.Robby.description}</p>
+                        <p className="bandmember-mobile-description-influence">Influenced by<br />{bandmembers.Robby.influences}</p>
+                        <p className="bandmember-mobile-description-funfact">Fun fact<br />{bandmembers.Robby.funFact}</p>
+                    </div>
+                </div>
+
+                <div className="bandmember-mobile">
+                    <img src={bandmembers.Laurens.img} />
+
+                    <div className="bandmember-mobile-description">
+                        <h3>{bandmembers.Laurens.name}</h3>
+                        <p className="bandmember-mobile-description-description">{bandmembers.Laurens.description}</p>
+                        <p className="bandmember-mobile-description-influence">Influenced by<br />{bandmembers.Laurens.influences}</p>
+                        <p className="bandmember-mobile-description-funfact">Fun fact<br />{bandmembers.Laurens.funFact}</p>
                     </div>
                 </div>
                 
