1bcc9710ab4a0c673bb15907ece40a0b92263e09
Jochen <jochen.panjaer@ziango.com>
2018-09-02 01:02:57 +0200

    adding video section

--- client/src/components/media/Media.js
+++ client/src/components/media/Media.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Modal from 'react-modal';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVideo} from '@fortawesome/free-solid-svg-icons';
+
 import albumEcho from '../../images/album-echoavenue.jpg';
 import albumBetween from '../../images/album-fewandfarbetween.jpg';
 import videoLeftField from '../../images/video-outofleftfield.jpg';
+import videoNostalgic from '../../images/video-nostalgicfordisaster.jpg';
+import videoCommon from '../../images/video-commonplace.jpg';
+import videoMourning from '../../images/video-mourningafter.jpg';
 
 class Media extends React.Component {
     constructor(props) {
@@ -53,6 +59,7 @@ class Media extends React.Component {
                                 <div className="music-album-details-tracklisting">
                                     
                                     <iframe src="https://open.spotify.com/embed/album/62QH1onVL0INqctURfgtej" 
+                                            title="Echo Avenue playlist on Spotify"
                                             frameBorder="0" 
                                             allowtransparency="true"
                                             allow="encrypted-media" />
@@ -93,9 +100,44 @@ class Media extends React.Component {
                     <h1>Videos</h1>
 
                     <div className="video-list">
-                        <a href="#" 
-                        onClick={() => this.openModal(<iframe width="560" height="315" src="https://www.youtube.com/embed/bA3STgJYs7A?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>)}>
+                        <a href="#video-oolf" 
+                           onClick={() => this.openModal(<iframe title="Out Of Left Field movie" width="560" height="315" src="https://www.youtube.com/embed/bA3STgJYs7A?rel=0&amp;showinfo=0" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>)}>
                             <img src={videoLeftField} alt="Video for out of left field" />
+
+                            <div className="overlay">
+                                <p>Watch 'Out Of Left Field'</p>
+                                <FontAwesomeIcon icon={faVideo} />
+                            </div>
+                        </a>
+
+                        <a href="#video-nostalgic" 
+                           onClick={() => this.openModal(<iframe width="560" height="315" src="https://www.youtube.com/embed/I0IOV27NcSA?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>)}>
+                            <img src={videoNostalgic} alt="Video for nostalgic for disaster" />
+
+                            <div className="overlay">
+                                <p>Watch 'Nostalgic For Disaster'</p>
+                                <FontAwesomeIcon icon={faVideo} />
+                            </div>
+                        </a>
+
+                        <a href="#video-commonplace" 
+                           onClick={() => this.openModal(<iframe width="560" height="315" src="https://www.youtube.com/embed/dLojSlbzoYo?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>)}>
+                            <img src={videoCommon} alt="Video for commonplace" />
+
+                            <div className="overlay">
+                                <p>Watch 'Live video for Commonplace'</p>
+                                <FontAwesomeIcon icon={faVideo} />
+                            </div>
+                        </a>
+
+                        <a href="#video-mourning" 
+                           onClick={() => this.openModal(<iframe width="560" height="315" src="https://www.youtube.com/embed/UeZyjwcHtp4?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>)}>
+                            <img src={videoMourning} alt="Video for mourning after" />
+
+                            <div className="overlay">
+                                <p>Watch 'Mourning After'</p>
+                                <FontAwesomeIcon icon={faVideo} />
+                            </div>
                         </a>
                     </div>
                 </div>
